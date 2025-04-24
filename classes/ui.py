4cf335242410b6b072e8fe95f0c7685670764276
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-04-24 12:26:14 -0700

    added text popup on pygame window

--- classes/ui.py
+++ classes/ui.py
@@ -3,7 +3,13 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
+font = pygame.font.Font('freesansbold.ttf', 32)
 running = True
+text = font.render("PySing!", True, (255, 255, 255))
+textRect = text.get_rect()
+textRect.center = (1280//2, 100)
+
+
 
 while running:
     for event in pygame.event.get():
@@ -11,7 +17,10 @@ while running:
             running = False
     
     screen.fill("black")
-        
+    
+    # render stuff
+    screen.blit(text, textRect)
+            
     # rendering game    
     pygame.display.flip()
     
