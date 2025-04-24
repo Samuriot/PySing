e2c1da13f540a8a60f1e0d2a004754b32a49f70e
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-04-24 12:32:54 -0700

    added mouse click event handling

--- classes/ui.py
+++ classes/ui.py
@@ -1,25 +1,31 @@
 import pygame
 
+def updateText(str):
+    text = font.render(str, True, (255, 255, 255))
+    textRect = text.get_rect()
+    textRect.center = (1280//2, 100)
+    return [text, textRect]
+
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 32)
 running = True
-text = font.render("PySing!", True, (255, 255, 255))
-textRect = text.get_rect()
-textRect.center = (1280//2, 100)
-
-
+text = updateText("PySing")
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            text = updateText("you clicked down")
+        if event.type == pygame.MOUSEBUTTONUP:
+            text = updateText("text reset")
     
     screen.fill("black")
     
     # render stuff
-    screen.blit(text, textRect)
+    screen.blit(text[0], text[1])
             
     # rendering game    
     pygame.display.flip()
