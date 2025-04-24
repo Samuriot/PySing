9c3d50ba6afe06c6071a47bb2d87c5353ebd1a92
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-04-24 12:53:34 -0700

    refactored UI.py to become a class object

--- classes/ui.py
+++ classes/ui.py
@@ -1,41 +1,46 @@
 import pygame
 
-def updateText(str):
-    text = font.render(str, True, (255, 255, 255))
-    textRect = text.get_rect()
-    textRect.center = (1280//2, 100)
-    return [text, textRect]
 
-pygame.init()
-screen = pygame.display.set_mode((1280,720))
-clock = pygame.time.Clock()
-font = pygame.font.Font('freesansbold.ttf', 32)
-running = True
-text = updateText("PySing")
-
-pygame.mixer.pre_init()
-pygame.mixer.init()
-
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            text = updateText("you clicked down and played music")
-            pygame.mixer.music.load("music/dabin.mp3")
-            pygame.mixer.music.play()
-        if event.type == pygame.MOUSEBUTTONUP:
-            text = updateText("text reset")
+class UI:
+    def updateText(self, str):
+        text = self.font.render(str, True, (255, 255, 255))
+        textRect = text.get_rect()
+        textRect.center = (1280//2, 100)
+        self.text = [text, textRect]
     
-    screen.fill("black")
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280,720))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.running = True
+        self.updateText("PySing")
+        pygame.mixer.pre_init()
+        pygame.mixer.init()
     
-    # render stuff
-    screen.blit(text[0], text[1])
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.updateText("you clicked down and played music")
+                    pygame.mixer.music.load("music/dabin.mp3")
+                    pygame.mixer.music.play()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.updateText("text reset")
             
-    # rendering game    
-    pygame.display.flip()
-    
-    clock.tick(60)
+                self.screen.fill("black")
+                
+                # render stuff
+                self.screen.blit(self.text[0], self.text[1])
+                        
+                # rendering game    
+                pygame.display.flip()
+                
+                self.clock.tick(60)
+
+game = UI()
+game.run()
 
 pygame.quit()
